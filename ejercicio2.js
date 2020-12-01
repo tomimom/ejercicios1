5bf0429f19ccd1dd443e890638bd095c42fe3f57
tomimom <75276601+tomimom@users.noreply.github.com>
2020-12-01 15:58:43 +0100

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,6 @@
+let a, b
+a = 2; b = 8
+resultado = a * b
+console.log(`Variable a contiene ${a} <br> ` )
+console.log(`Variable b contiene ${b} <br> ` )
+console.log(`El producto de a por b es ${resultado} `)
